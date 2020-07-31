bc9da1c776a120c05c02a016bb4ffaa84b8d6ecc
Dara Kimberly <darakimberlys@hotmail.com>
2020-07-31 18:15:44 -0300

    solved

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function FormField({value, type, name, onChange, label}) {
-    return(
-        <div>
-        <label>
-            {label}
-            <input
-            label={label}
-            type={type}
-            name={name}
-            value={value}
-            onChange={onChange}
-            />
-        </label>
+function FormField({
+  label, type, name, value, onChange,
+}) {
+  const fieldId = `id_${name}`;
+  return (
+    <div>
+      <label
+        htmlFor={fieldId}
+      >
+        {label}
+        :
+        <input
+          id={fieldId}
+          label={label}
+          type={type}
+          name={name}
+          value={value}
+          onChange={onChange}
+        />
+      </label>
     </div>
-    )
+  );
 }
+
+FormField.defaultProps = {
+  type: 'text',
+  value: '',
+  onChange: () => {},
+};
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
 
 export default FormField;
